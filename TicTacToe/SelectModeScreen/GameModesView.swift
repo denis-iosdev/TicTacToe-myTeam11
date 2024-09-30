055aa08426853a610dc5d80b1b3f59acf2caa755
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-30 18:24:45 +0200

    Created GameModesView with directory

---
+++ TicTacToe/SelectModeScreen/GameModesView.swift
@@ -0,0 +1,18 @@
+//
+//  GameModesView.swift
+//  TicTacToe
+//
+//  Created by Иван Семикин on 30/09/2024.
+//
+
+import SwiftUI
+
+struct GameModesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameModesView()
+}
