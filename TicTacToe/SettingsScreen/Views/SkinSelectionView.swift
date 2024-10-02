304e38c3e0615e2232ab2b0fc86e14c9355d870f
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-02 16:13:17 +0200

    Added skinImages to project

---
+++ TicTacToe/SettingsScreen/Views/SkinSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SkinSelectionView.swift
+//  TicTacToe
+//
+//  Created by Иван Семикин on 02/10/2024.
+//
+
+import SwiftUI
+
+struct SkinSelectionView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    SkinSelectionView()
+}
