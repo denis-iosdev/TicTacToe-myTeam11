c0f4473d80663486946e5a033c596fc766c025f0
Pavel Gritskov <pavel_ios_dev@mobilexapps.com>
2024-10-03 15:23:38 +0400

    FEAT: add router

---
+++ TicTacToe/Common/Router.swift
@@ -0,0 +1,56 @@
+//
+//  Router.swift
+//  TicTacToe
+//
+//  Created by Pavel Gritskov on 03.10.24.
+//
+
+import Foundation
+
+enum Router: Hashable {
+    case onbording
+    case help
+    case setting
+    case gameMod
+    case game(Bool)
+    case difflvl
+    case leaderboard
+    case result(ResultGame)
+}
+
+struct ResultGame: Hashable {
+    let name: String
+    let image: String
+}
+
+// window
+// --> Onbording
+// Onbording
+// --> help
+// --> setting
+// --> gameMod
+
+// help
+// <-- (back) Onbording
+// setting
+// <-- (back) Onbording
+
+// gameMod
+// <-- (back) Onbording
+// --> setting
+// --> (ture) game
+// --> (false) game
+// --> difflvl (fullScreen) (под вопросом как будет работать переход назад)
+// --> leaderboard
+
+// setting
+// <-- (back) gameMod
+// difflvl
+// <-- (back) gameMod
+// leaderboard
+// <-- (back) gameMod
+
+// game
+// <-- (back) gameMod
+// --> result (String, GameResult)
+
