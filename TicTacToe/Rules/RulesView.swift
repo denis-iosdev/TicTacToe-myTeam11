21234b7553c5f1e511e251ae148e6d69d25546d1
denisgindulin <denisgindulin@me.com>
2024-10-04 21:56:36 +0300

    HOTFIX: change padding bellow nav bar

--- TicTacToe/Rules/RulesView.swift
+++ TicTacToe/Rules/RulesView.swift
@@ -18,24 +18,26 @@ struct RulesView: View {
                   blue: 1)
             .ignoresSafeArea()
             
-            ScrollView(showsIndicators: false) {
-                RuleItem(number: "1",
-                         text: "Draw a grid with three rows and three columns, creating nine squares in total.")
-                
-                RuleItem(number: "2",
-                         text: "Players take turns placing their marker (X or O) in an empty square.\nTo make a move, a player selects a number corresponding to the square where they want to place their marker.")
-                
-                RuleItem(number: "3",
-                         text: "Player X starts by choosing a square (e.g., square 5).\nPlayer O follows by choosing an empty square (e.g., square 1).\nContinue alternating turns until the game ends.")
-                
-                RuleItem(number: "4",
-                         text: "The first player to align three of their markers horizontally, vertically, or diagonally wins.\nExamples of Winning Combinations:\nHorizontal: Squares 1, 2, 3 or 4, 5, 6 or 7, 8, 9\nVertical: Squares 1, 4, 7 or 2, 5, 8 or 3, 6, 9\nDiagonal: Squares 1, 5, 9 or 3, 5, 7.")
-            }
-            .padding(.top, 24)
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarBackButtonHidden()
-            .toolbar {
-                ToolBarNavigationItems(title: "How to play", leftAction: { navigator.pop() })
+            ScrollView(showsIndicators: false){
+            VStack {
+                    RuleItem(number: "1",
+                             text: "Draw a grid with three rows and three columns, creating nine squares in total.")
+                    
+                    RuleItem(number: "2",
+                             text: "Players take turns placing their marker (X or O) in an empty square.\nTo make a move, a player selects a number corresponding to the square where they want to place their marker.")
+                    
+                    RuleItem(number: "3",
+                             text: "Player X starts by choosing a square (e.g., square 5).\nPlayer O follows by choosing an empty square (e.g., square 1).\nContinue alternating turns until the game ends.")
+                    
+                    RuleItem(number: "4",
+                             text: "The first player to align three of their markers horizontally, vertically, or diagonally wins.\nExamples of Winning Combinations:\nHorizontal: Squares 1, 2, 3 or 4, 5, 6 or 7, 8, 9\nVertical: Squares 1, 4, 7 or 2, 5, 8 or 3, 6, 9\nDiagonal: Squares 1, 5, 9 or 3, 5, 7.")
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarBackButtonHidden()
+                .toolbar {
+                    ToolBarNavigationItems(title: "How to play", leftAction: { navigator.pop() })
+                }
+                .padding(.top, 24)
             }
         }
     }
